fd5c8048147698029efd05b47e6f7083347dcfab
Soon-Kwon <koentc010@gmail.com>
2021-10-25 22:09:17 +0900

    controller 구현

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -2,14 +2,19 @@ package com.example.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TodoDTO;
+import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
 
 @RestController
@@ -27,4 +32,38 @@ public class TodoController {
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return ResponseEntity.ok().body(response);
 	}
+	
+	@PostMapping
+	public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
+		try {
+			String temporaryUserId = "temporary-user";
+			
+			// (1) TodoEntity로 변환한다
+			TodoEntity entity = TodoDTO.toEntity(dto);
+			
+			// (2) id를 null로 초기화한다. 생성 당시에는 id가 없어야하기 때문이다.
+			entity.setId(null);
+			
+			// (3) 임시 사용자 아이디를 설정해준다. 이 부분은 인증과 인가에서 수정할 예정
+			// 로그인 없이 사용할 수 있는 애플리케이션이다 현재는.
+			entity.setUserId(temporaryUserId);
+			
+			// (4) 서비스를 이용해 Todo 엔티티를 생성한다.
+			List<TodoEntity> entities = service.create(entity);
+			
+			// (5) 자바 스트림을 이용해 리턴된 ResponseDTO를 초기화한다.
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+			
+			// (6) 변환된 Todo 리스트를 이용해 ResponseDTO를 초기화한다.
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+			
+			return ResponseEntity.ok().body(response);
+		
+		} catch (Exception e) {
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}
+	}
+	
 }
--- src/main/java/com/example/demo/dto/TodoDTO.java
+++ src/main/java/com/example/demo/dto/TodoDTO.java
@@ -3,22 +3,31 @@ package com.example.demo.dto;
 import com.example.demo.model.TodoEntity;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class TodoDTO {
+	// 필드의 이름을 모델과 다르게 짓는다면 캡슐화 할 수 있다. 
 	private String id;
 	private String title;
 	private boolean done;
 	
 	public TodoDTO(final TodoEntity entity) {
+		// 원래는 Model 객체가 와야 되지만, 현재 프로젝트에서는 
+		// Entity 객체가 Model의 역할도 함께 수행하고 있다. 
 		this.id = entity.getId();
 		this.title = entity.getTitle();
 		this.done = entity.isDone();
+	}
+	
+	public static TodoEntity toEntity(final TodoDTO dto) {
+		return TodoEntity.builder()
+				.id(dto.getId())
+				.title(dto.getTitle())
+				.done(dto.isDone())
+				.build();
 	}
 }
